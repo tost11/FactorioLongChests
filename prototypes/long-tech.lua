23b575fb5b4b1cafe053066ae14e1e257d6cb7db
Lukas Hagenauer <tost@tost-soft.de>
2019-06-09 22:10:56 +0200

    fix for factorio version 1.17

--- prototypes/long-tech.lua
+++ prototypes/long-tech.lua
@@ -35,8 +35,8 @@ data:extend(
         count = 75,
         ingredients =
           {
-            {"science-pack-1", 1},
-            {"science-pack-2", 1}
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1}
           },
         time = 30
 		}
